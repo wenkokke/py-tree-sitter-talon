aa1d45f0854013e01db25078067131f4e646375a
Wen Kokke <wenkokke@users.noreply.github.com>
2022-07-12 20:31:52 +0100

    Update submodule on sdist

--- setup.py
+++ setup.py
@@ -2,12 +2,20 @@
 Py-Tree-Sitter-Talon
 """
 
-from os import path
+from os import path, system
 from setuptools import setup
+from setuptools.command.sdist import sdist
+
+
+class custom_sdist(sdist):
+    def run(self):
+        system("git submodule update --init")
+        sdist.run(self)
 
 
 with open(path.join(path.dirname(__file__), "README.md")) as f:
     LONG_DESCRIPTION = f.read()
+
 
 setup(
     name="tree_sitter_talon",
@@ -34,4 +42,5 @@ setup(
         "tree_sitter",
         "tree_sitter_type_provider @ git+ssh://git@github.com/wenkokke/py-tree-sitter-type-provider.git#egg=tree_sitter_type_provider",
     ],
+    cmdclass={"sdist": custom_sdist},
 )
