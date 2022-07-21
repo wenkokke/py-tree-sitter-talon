069262d93de6d148df993dfbcc6623cdbab45f6e
Wen Kokke <wenkokke@users.noreply.github.com>
2022-07-21 19:16:28 +0100

    Update tests, Python 3.9 compat

--- tree_sitter_talon/__init__.py
+++ tree_sitter_talon/__init__.py
@@ -1,12 +1,12 @@
 from pathlib import Path
-from typing import Sequence
-from tree_sitter_type_provider import TreeSitterTypeProvider
-from tree_sitter_type_provider.node_types import NodeType, Node, Point, ERROR
+from tree_sitter_type_provider import TreeSitterTypeProvider, NodeType, Node, Point, ERROR
 from pkg_resources import resource_filename  # type: ignore
 
 import sys
 import tree_sitter as ts
+import typing
 import os
+
 
 class TreeSitterTalon(TreeSitterTypeProvider):
     def resource_path(self, resource_name: str) -> Path:
@@ -48,7 +48,7 @@ class TreeSitterTalon(TreeSitterTypeProvider):
     def __init__(self):
         # Read node-types.json
         node_types = NodeType.schema().loads(
-            self.node_types_path.read_text(), many=True
+            self.node_types_path.read_text(encoding="utf-8"), many=True
         )
 
         # Conversion from tree-sitter names to Python names
@@ -72,26 +72,33 @@ class TreeSitterTalon(TreeSitterTypeProvider):
 
     def parse(
         self,
-        contents: str | bytes,
-        has_header: bool | None = None,
+        contents: typing.Union[str, bytes],
+        *,
+        has_header: typing.Optional[bool] = None,
         encoding: str = "utf-8",
-    ) -> Sequence[Node] | Node | None:
-        tree = self.parse_as_tree_sitter(contents, has_header, encoding)
+    ) -> typing.Union[typing.Sequence[Node], Node, None]:
+        tree = self.parse_as_tree_sitter(
+            contents, has_header=has_header, encoding=encoding
+        )
         return self.from_tree_sitter(tree.root_node)
 
     def parse_file(
         self,
-        path: str | Path,
-        has_header: bool | None = None,
+        path: typing.Union[str, Path],
+        *,
+        has_header: typing.Optional[bool] = None,
         encoding: str = "utf-8",
-    ) -> Sequence[Node] | Node | None:
-        tree = self.parse_file_as_tree_sitter(path, has_header, encoding)
+    ) -> typing.Union[typing.Sequence[Node], Node, None]:
+        tree = self.parse_file_as_tree_sitter(
+            path, has_header=has_header, encoding=encoding
+        )
         return self.from_tree_sitter(tree.root_node)
 
     def parse_as_tree_sitter(
         self,
-        contents: str | bytes,
-        has_header: bool | None = None,
+        contents: typing.Union[str, bytes],
+        *,
+        has_header: typing.Optional[bool] = None,
         encoding: str = "utf-8",
     ) -> ts.Tree:
         if isinstance(contents, str):
@@ -104,14 +111,17 @@ class TreeSitterTalon(TreeSitterTypeProvider):
 
     def parse_file_as_tree_sitter(
         self,
-        path: str | Path,
-        has_header: bool | None = None,
+        path: typing.Union[str, Path],
+        *,
+        has_header: typing.Optional[bool] = None,
         encoding: str = "utf-8",
     ) -> ts.Tree:
         if not isinstance(path, Path):
             path = Path(path)
         contents = path.read_bytes()
-        return self.parse_as_tree_sitter(contents, has_header, encoding)
+        return self.parse_as_tree_sitter(
+            contents, has_header=has_header, encoding=encoding
+        )
 
 
 sys.modules[__name__] = TreeSitterTalon()
