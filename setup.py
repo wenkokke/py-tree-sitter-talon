59ca47341a898dc67197d8ae5f84a11ed69c9c6a
Wen Kokke <wenkokke@users.noreply.github.com>
2022-08-03 18:00:06 +0100

    Bump version 1.5.20 -> 1.5.21

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ with open(os.path.join(os.path.dirname(__file__), "README.md")) as f:
 
 setuptools.setup(
     name="tree_sitter_talon",
-    version="1.5.20",
+    version="1.5.21",
     description="Parser for Talon files in Python",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
