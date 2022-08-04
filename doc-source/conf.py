3b850bc2bd7619d5d254e6f869d4a8500a58a3cc
Wen Kokke <wenkokke@users.noreply.github.com>
2022-08-04 16:03:48 +0100

    Bump version 1.6.1 -> 1.6.2

--- doc-source/conf.py
+++ doc-source/conf.py
@@ -24,7 +24,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "tree_sitter_talon"
 copyright = "2022, Wen Kokke"
 author = "Wen Kokke"
-release = "1.6.1"
+release = "1.6.2"
 
 # -- General configuration ---------------------------------------------------
 
