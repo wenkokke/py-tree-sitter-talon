7c6e6b842c12563e05aa498e94fc3448dc716640
Wen Kokke <wenkokke@users.noreply.github.com>
2022-08-04 16:12:24 +0100

    Create util module

--- tree_sitter_talon/util.py
+++ tree_sitter_talon/util.py
@@ -1,15 +1,13 @@
 import typing
 
-import core
+from .core import TreeSitterTalon
 
 
-def set_library(library_path: str) -> None:
-    core.TreeSitterTalon.library_path = library_path
-
-
-def find_library(extra_library_path: typing.Optional[str] = None) -> str:
-    return core.TreeSitterTalon.find_library(extra_library_path)
+def find_library(
+    extra_library_path: typing.Optional[str] = None,
+) -> typing.Optional[str]:
+    return TreeSitterTalon.find_library(extra_library_path)
 
 
 def build_library(library_path: typing.Optional[str] = None) -> str:
-    return core.TreeSitterTalon.build_library(library_path)
+    return TreeSitterTalon.build_library(library_path)
