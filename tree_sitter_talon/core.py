b13e50e5c086cac88a6da91356715b079c9ca875
Wen Kokke <wenkokke@users.noreply.github.com>
2022-08-06 23:02:42 +0100

    Bump version 1.6.16 -> 1.6.17

--- tree_sitter_talon/core.py
+++ tree_sitter_talon/core.py
@@ -18,7 +18,7 @@ class TalonLanguage(tree_sitter.Language):
 
 class TreeSitterTalon(tree_sitter_type_provider.TreeSitterTypeProvider):
 
-    __version__: str = "1.6.16"
+    __version__: str = "1.6.17"
 
     _library_path: typing.Optional[str] = None
 
