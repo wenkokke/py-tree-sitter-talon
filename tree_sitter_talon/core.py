dbb007e0b5b973ffd70a4d4923e5b7b658906166
Wen Kokke <wenkokke@users.noreply.github.com>
2022-08-08 19:59:23 +0100

    Bump version 1.8.1 -> 1.8.2

--- tree_sitter_talon/core.py
+++ tree_sitter_talon/core.py
@@ -18,7 +18,7 @@ class TalonLanguage(tree_sitter.Language):
 
 class TreeSitterTalon(tree_sitter_type_provider.TreeSitterTypeProvider):
 
-    __version__: str = "1.8.1"
+    __version__: str = "1.8.2"
 
     _library_path: typing.Optional[str] = None
 
