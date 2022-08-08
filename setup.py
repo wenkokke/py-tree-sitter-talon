005f530828a77a251bbf74066130596db3132d7e
Wen Kokke <wenkokke@users.noreply.github.com>
2022-08-08 22:15:00 +0100

    Remove tox, just call build_ext

--- setup.py
+++ setup.py
@@ -32,7 +32,6 @@ setuptools.setup(
     tests_require=[
         "pytest >=7.1.2, <8",
         "pytest-golden >=0.2.2, <0.3",
-        "tox >=3.25.1, <4",
     ],
     install_requires=[
         "tree_sitter ==0.20.0",
@@ -52,7 +51,6 @@ setuptools.setup(
         "test": [
             "pytest >=7.1.2, <8",
             "pytest-golden >=0.2.2, <0.3",
-            "tox >=3.25.1, <4",
         ],
     },
     ext_modules=[
