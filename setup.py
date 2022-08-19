ec692b0bea462ab1752deca8685387adf9b4ed9f
Wen Kokke <wenkokke@users.noreply.github.com>
2022-08-19 21:51:58 +0100

    Bump tree_sitter_type_provider

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setuptools.setup(
     ],
     install_requires=[
         "tree_sitter ==0.20.0",
-        "tree_sitter_type_provider ==2.1.0",
+        "tree_sitter_type_provider ==2.1.2",
         "dataclasses-json >=0.5.7, <0.6",
     ],
     extras_require={
