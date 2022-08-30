0732bdd59548120ab2416b846e8ec91841e7a0d0
Wen Kokke <wenkokke@users.noreply.github.com>
2022-08-30 13:47:16 +0100

    Bump version 3.0.4 -> 3.0.5

--- tree_sitter_talon/core.py
+++ tree_sitter_talon/core.py
@@ -18,7 +18,7 @@ class TalonLanguage(tree_sitter.Language):
 
 class TreeSitterTalon(tree_sitter_type_provider.TreeSitterTypeProvider):
 
-    __version__: str = "3.0.4"
+    __version__: str = "3.0.5"
 
     _library_path: typing.Optional[str] = None
 
