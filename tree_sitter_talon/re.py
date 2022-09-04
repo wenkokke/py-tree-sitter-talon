fd9bc3dbeb2dc9a8cb1c7e58f54339df1b0bfd5a
Wen Kokke <wenkokke@users.noreply.github.com>
2022-09-04 16:37:31 +0100

    Add lookup for captures and lists

--- tree_sitter_talon/re.py
+++ tree_sitter_talon/re.py
@@ -1,3 +1,4 @@
+import collections.abc
 import re
 import typing
 
@@ -32,11 +33,19 @@ TalonRuleTop = typing.Union[
     TalonWord,
 ]
 
+TalonCaptureLookup = collections.abc.Callable[[str], typing.Optional[TalonRuleTop]]
+TalonListLookup = collections.abc.Callable[[str], typing.Optional[list[str]]]
+
 
 def compile(
-    rule: TalonRuleTop, *, captures: dict[str, str] = {}, lists: dict[str, str] = {}
+    rule: TalonRuleTop,
+    *,
+    captures: typing.Optional[TalonCaptureLookup] = None,
+    lists: typing.Optional[TalonListLookup] = None,
 ) -> re.Pattern[str]:
-    return re.compile(_compile_str(rule, captures=captures, lists=lists))
+    captures = captures or (lambda capture_name: None)
+    lists = lists or (lambda list_name: None)
+    return re.compile(_pattern_str(rule, captures=captures, lists=lists))
 
 
 def _get_only_child(
@@ -49,48 +58,55 @@ def _get_only_child(
     raise ValueError(children)
 
 
-def _compile_str(
-    rule: TalonRuleTop, *, captures: dict[str, str], lists: dict[str, str]
+def _pattern_str(
+    rule: TalonRuleTop, *, captures: TalonCaptureLookup, lists: TalonListLookup
 ) -> str:
     if isinstance(rule, TalonCapture):
         capture_name = rule.capture_name.text.strip()
-        capture_text = re.escape(rf"<{capture_name}>")
-        return captures.get(capture_name, capture_text)
+        capture_rule = captures(capture_name)
+        if capture_rule:
+            return _pattern_str(capture_rule, captures=captures, lists=lists)
+        else:
+            return re.escape(rf"<{capture_name}>")
     elif isinstance(rule, TalonChoice):
-        rule_compile_str = r"|".join(
+        pattern_str = r"|".join(
             [
-                _compile_str(child, captures=captures, lists=lists)
+                _pattern_str(child, captures=captures, lists=lists)
                 for child in rule.children
                 if not isinstance(child, TalonComment)
             ]
         )
-        return rf"({rule_compile_str})"
+        return rf"({pattern_str})"
     elif isinstance(rule, TalonEndAnchor):
         return r"$"
     elif isinstance(rule, TalonList):
         list_name = rule.list_name.text.strip()
-        list_text = re.escape(rf"<{list_name}>")
-        return lists.get(list_name, list_text)
+        list_items = lists(list_name)
+        if list_items:
+            list_item_pattern = r"|".join(list_items)
+            return rf"({list_item_pattern})"
+        else:
+            return re.escape(rf"<{list_name}>")
     elif isinstance(rule, TalonOptional):
         child = _get_only_child(rule.children)
-        rule_compile_str = _compile_str(child, captures=captures, lists=lists)
-        return rf"({rule_compile_str})?"
+        pattern_str = _pattern_str(child, captures=captures, lists=lists)
+        return rf"({pattern_str})?"
     elif isinstance(rule, TalonParenthesizedRule):
         child = _get_only_child(rule.children)
-        rule_compile_str = _compile_str(child, captures=captures, lists=lists)
-        return rf"({rule_compile_str})"
+        pattern_str = _pattern_str(child, captures=captures, lists=lists)
+        return rf"({pattern_str})"
     elif isinstance(rule, TalonRepeat):
         child = _get_only_child(rule.children)
-        rule_compile_str = _compile_str(child, captures=captures, lists=lists)
-        return rf"({rule_compile_str})*"
+        pattern_str = _pattern_str(child, captures=captures, lists=lists)
+        return rf"({pattern_str})*"
     elif isinstance(rule, TalonRepeat1):
         child = _get_only_child(rule.children)
-        rule_compile_str = _compile_str(child, captures=captures, lists=lists)
-        return rf"({rule_compile_str})+"
+        pattern_str = _pattern_str(child, captures=captures, lists=lists)
+        return rf"({pattern_str})+"
     elif isinstance(rule, TalonRule):
         return r"\s+".join(
             [
-                _compile_str(child, captures=captures, lists=lists)
+                _pattern_str(child, captures=captures, lists=lists)
                 for child in rule.children
                 if not isinstance(child, TalonComment)
             ]
@@ -98,7 +114,7 @@ def _compile_str(
     elif isinstance(rule, TalonSeq):
         return r"\s+".join(
             [
-                _compile_str(child, captures=captures, lists=lists)
+                _pattern_str(child, captures=captures, lists=lists)
                 for child in rule.children
                 if not isinstance(child, TalonComment)
             ]
