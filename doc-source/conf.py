78c62a40118dd7f7ccd2b276d27aa0d932d88e59
Wen Kokke <wenkokke@users.noreply.github.com>
2022-09-05 13:27:03 +0100

    Bump version 3.1.0 -> 3.1.1

--- doc-source/conf.py
+++ doc-source/conf.py
@@ -24,7 +24,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "tree_sitter_talon"
 copyright = "2022, Wen Kokke"
 author = "Wen Kokke"
-release = "3.1.0"
+release = "3.1.1"
 
 # -- General configuration ---------------------------------------------------
 
--- tree_sitter_talon/core.py
+++ tree_sitter_talon/core.py
@@ -18,7 +18,7 @@ class TalonLanguage(tree_sitter.Language):
 
 class TreeSitterTalon(tree_sitter_type_provider.TreeSitterTypeProvider):
 
-    __version__: str = "3.1.0"
+    __version__: str = "3.1.1"
 
     _library_path: typing.Optional[str] = None
 
