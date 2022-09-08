9db08b7cd64d704f7f8908f48cad3bffb8ddd64d
Wen Kokke <wenkokke@users.noreply.github.com>
2022-09-08 17:46:06 +0100

    Add mypy

--- Makefile
+++ Makefile
@@ -13,12 +13,9 @@ html: Makefile
 
 # Tests
 
-test: pytest stubtest
-
-pytest:
-	@python setup.py pytest
-
-stubtest:
+test:
+	@python setup.py build_ext
+	@PYTHONPATH=. pytest
 	@PYTHONPATH=. stubtest --mypy-config-file pyproject.toml tree_sitter_talon
 
 .PHONY: test pytest stubtest
--- setup.py
+++ setup.py
@@ -4,26 +4,26 @@ from setuptools.command.install import install  # isort:skip
 from setuptools.command.sdist import sdist  # isort:skip
 import distutils.cmd
 import distutils.extension
-import os
 import pathlib
 import subprocess
 import sys
 
+pkg_dir = pathlib.Path(__file__).parent
+
 
 def _get_description() -> str:
-    with open(os.path.join(os.path.dirname(__file__), "README.md")) as fh:
-        return fh.read()
+    return (pkg_dir / "README.md").read_text()
 
 
 def _get_version() -> str:
-    sys.path.insert(0, os.path.abspath("."))
+    sys.path.insert(0, str(pkg_dir.absolute()))
     import tree_sitter_talon.version as pkg
 
     return pkg.__version__
 
 
 def _get_data_files() -> list[str]:
-    pkg_path = pathlib.Path(__file__).parent / "tree_sitter_talon"
+    pkg_path = pkg_dir / "tree_sitter_talon"
     return [
         str(data_file.relative_to(pkg_path))
         for data_file in pkg_path.glob("data/tree-sitter-talon/**/*")
@@ -77,10 +77,8 @@ setuptools.setup(
     keywords=["talon", "parser"],
     platforms=["any"],
     python_requires=">=3.9",
-    setup_requires=[
-        "pytest-runner",
-    ],
     tests_require=[
+        "mypy >=0.971, <1",
         "pytest >=7.1.2, <8",
         "pytest-golden >=0.2.2, <0.3",
     ],
@@ -92,6 +90,7 @@ setuptools.setup(
     extras_require={
         "dev": [
             "build >=0.8.0, <0.9",
+            "tox >=3.25.1, <4",
             "twine >=4.0.1, <5",
         ],
         "docs": [
