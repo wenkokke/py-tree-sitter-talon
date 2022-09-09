0d72ad278c9b8e2e3e16066fc9770cb32e8ba383
Wen Kokke <wenkokke@users.noreply.github.com>
2022-09-09 21:04:38 +0100

    Fix conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "tree_sitter_talon"
 copyright = "2022, Wen Kokke"
 author = "Wen Kokke"
-release = "1!1000.2.0.0"
+release = "1000.2.0.0"
 
 # -- General configuration ---------------------------------------------------
 
