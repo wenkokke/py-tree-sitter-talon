d2fe822336ad997317d2e2c5956126199f056db1
Wen Kokke <wenkokke@users.noreply.github.com>
2022-09-24 16:45:35 +0100

    Bump build number 1002 -> 1003

--- tree_sitter_talon/__init__.py
+++ tree_sitter_talon/__init__.py
@@ -74,7 +74,7 @@ from .internal.parsec import match as match
 # TODO: Switch to "1!{tree-sitter-talon.version}.{build}".
 #       Blocked on <python-poetry/poetry/issues/6466>.
 
-__version__: str = "1002.3.1.0"
+__version__: str = "1003.3.1.0"
 #                   ^^^^ build
 #                        ^^^^^ tree-sitter-talon version
 
