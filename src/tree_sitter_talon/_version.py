4002414b24ac0bbb08d22bda004246b60fdf6a1e
Wen Kokke <wenkokke@users.noreply.github.com>
2023-05-17 17:50:45 +0100

    Switch to SemVer

--- src/tree_sitter_talon/_version.py
+++ src/tree_sitter_talon/_version.py
@@ -7,22 +7,18 @@ import importlib_resources
 # Version Number
 ################################################################################
 
-# The build number.
-__build__: int = 1007
-
 # The tree-sitter-talon version.
 try:
-    __tree_sitter_talon_version__: str = json.loads(
+    TREE_SITTER_TALON_VERSION: str = json.loads(
         importlib_resources.files("tree_sitter_talon")
         .joinpath("_tree_sitter_talon", "package.json")
         .read_text(encoding="utf-8")
     )["version"]
 except ModuleNotFoundError:
-    __tree_sitter_talon_version__: str = json.loads(  # type: ignore[no-redef]
+    TREE_SITTER_TALON_VERSION_: str = json.loads(  # type: ignore[no-redef]
         (
             pathlib.Path(__file__).parent / "_tree_sitter_talon" / "package.json"
         ).read_text(encoding="utf-8")
     )["version"]
 
-# The py-tree-sitter-talon version.
-VERSION: str = f"{__build__}.{__tree_sitter_talon_version__}"
+VERSION: str = f"1!1.0.0+grammar{TREE_SITTER_TALON_VERSION_}"
