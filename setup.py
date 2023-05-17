7397a2846b1e6fb65510ecae44d334772cb1741e
Wen Kokke <wenkokke@users.noreply.github.com>
2023-05-17 15:40:51 +0100

    Simplify setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,17 @@
 from setuptools import Extension, setup
 
-ext_module = Extension(
-    name="tree_sitter_talon._internal.binding",
-    sources=[
-        "src/tree_sitter_talon/_internal/binding.c",
-        "src/tree_sitter_talon/_tree_sitter_talon/src/parser.c",
-        "src/tree_sitter_talon/_tree_sitter_talon/src/scanner.cc",
-    ],
-    include_dirs=[
-        "src/tree_sitter_talon/_tree_sitter_talon/src",
+setup(
+    ext_modules=[
+        Extension(
+            name="tree_sitter_talon._internal.binding",
+            sources=[
+                "src/tree_sitter_talon/_internal/binding.c",
+                "src/tree_sitter_talon/_tree_sitter_talon/src/parser.c",
+                "src/tree_sitter_talon/_tree_sitter_talon/src/scanner.cc",
+            ],
+            include_dirs=[
+                "src/tree_sitter_talon/_tree_sitter_talon/src",
+            ],
+        )
     ],
 )
-
-
-def main():
-    setup(
-        ext_modules=[ext_module],
-    )
-
-
-if __name__ == "__main__":
-    main()
