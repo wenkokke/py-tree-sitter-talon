a92f9ebdf4f0821f44d0a15699296cbaec9d0ed8
Wen Kokke <wenkokke@users.noreply.github.com>
2023-05-17 15:38:56 +0100

    Private packages

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import Extension, setup
 
 ext_module = Extension(
-    name="tree_sitter_talon.internal.binding",
+    name="tree_sitter_talon._internal.binding",
     sources=[
-        "tree_sitter_talon/internal/binding.c",
-        "tree_sitter_talon/data/tree-sitter-talon/src/parser.c",
-        "tree_sitter_talon/data/tree-sitter-talon/src/scanner.cc",
+        "src/tree_sitter_talon/_internal/binding.c",
+        "src/tree_sitter_talon/_tree_sitter_talon/src/parser.c",
+        "src/tree_sitter_talon/_tree_sitter_talon/src/scanner.cc",
     ],
     include_dirs=[
-        "tree_sitter_talon/data/tree-sitter-talon/src",
+        "src/tree_sitter_talon/_tree_sitter_talon/src",
     ],
 )
 
