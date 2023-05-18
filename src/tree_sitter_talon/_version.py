e2610dd9c3d5e3ac292a555a77a27c5bf0602181
Wen Kokke <wenkokke@users.noreply.github.com>
2023-05-18 10:33:39 +0100

    Fix bug in stubs

--- src/tree_sitter_talon/_version.py
+++ src/tree_sitter_talon/_version.py
@@ -9,9 +9,8 @@ import packaging.version
 ################################################################################
 
 # The tree-sitter-talon version.
-TREE_SITTER_TALON: packaging.version.Version
 try:
-    TREE_SITTER_TALON_VERSION = packaging.version.Version(
+    TREE_SITTER_TALON_VERSION: packaging.version.Version = packaging.version.Version(
         json.loads(
             importlib_resources.files("tree_sitter_talon")
             .joinpath("_tree_sitter_talon", "package.json")
@@ -19,8 +18,8 @@ try:
         )["version"]
     )
 except ModuleNotFoundError:
-    TREE_SITTER_TALON_VERSION = packaging.version.Version(
-        json.loads(  # type: ignore[no-redef]
+    TREE_SITTER_TALON_VERSION: packaging.version.Version = packaging.version.Version(  # type: ignore[no-redef]
+        json.loads(
             (
                 pathlib.Path(__file__).parent / "_tree_sitter_talon" / "package.json"
             ).read_text(encoding="utf-8")
