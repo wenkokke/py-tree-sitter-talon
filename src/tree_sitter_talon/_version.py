f8b06dcf8cfa3dded0a79cba823946e807c8d3c1
Wen Kokke <wenkokke@users.noreply.github.com>
2023-05-18 10:25:31 +0100

    Remove local versions

--- src/tree_sitter_talon/_version.py
+++ src/tree_sitter_talon/_version.py
@@ -2,23 +2,29 @@ import json
 import pathlib
 
 import importlib_resources
+import packaging.version
 
 ################################################################################
 # Version Number
 ################################################################################
 
 # The tree-sitter-talon version.
+TREE_SITTER_TALON: packaging.version.Version
 try:
-    TREE_SITTER_TALON_VERSION: str = json.loads(
-        importlib_resources.files("tree_sitter_talon")
-        .joinpath("_tree_sitter_talon", "package.json")
-        .read_text(encoding="utf-8")
-    )["version"]
+    TREE_SITTER_TALON_VERSION = packaging.version.Version(
+        json.loads(
+            importlib_resources.files("tree_sitter_talon")
+            .joinpath("_tree_sitter_talon", "package.json")
+            .read_text(encoding="utf-8")
+        )["version"]
+    )
 except ModuleNotFoundError:
-    TREE_SITTER_TALON_VERSION: str = json.loads(  # type: ignore[no-redef]
-        (
-            pathlib.Path(__file__).parent / "_tree_sitter_talon" / "package.json"
-        ).read_text(encoding="utf-8")
-    )["version"]
+    TREE_SITTER_TALON_VERSION = packaging.version.Version(
+        json.loads(  # type: ignore[no-redef]
+            (
+                pathlib.Path(__file__).parent / "_tree_sitter_talon" / "package.json"
+            ).read_text(encoding="utf-8")
+        )["version"]
+    )
 
-VERSION: str = f"1!1.0.6+grammar{TREE_SITTER_TALON_VERSION}"
+VERSION: str = f"{TREE_SITTER_TALON_VERSION.major}!1.0"
